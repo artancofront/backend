0bc64e253961154f0f944d88c9c07053657bd536
Hamid <tavalla.hamid@gmail.com>
2025-04-06 12:41:49 +0330

    app service provider edited

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,16 @@
 
 namespace App\Providers;
 
+use App\Models\CategoryDiscount;
+use App\Models\Order;
+use App\Models\ProductCommentScore;
+use App\Models\ProductConversation;
+use App\Models\ProductVariant;
+use App\Observers\CategoryDiscountObserver;
+use App\Observers\OrderObserver;
+use App\Observers\ProductCommentScoreObserver;
+use App\Observers\ProductConversationObserver;
+use App\Observers\ProductVariantObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +34,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Order::observe(OrderObserver::class);
+        ProductVariant::observe(ProductVariantObserver::class);
+        ProductCommentScore::observe(ProductCommentScoreObserver::class);
+        ProductConversation::observe(ProductConversationObserver::class);
+        CategoryDiscount::observe(CategoryDiscountObserver::class);
+
     }
 }
