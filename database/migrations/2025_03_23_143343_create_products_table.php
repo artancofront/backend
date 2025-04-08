0ba415ffd801374c607dd9432ceb94bd667ce15f
Hamid <tavalla.hamid@gmail.com>
2025-04-09 00:08:17 +0330

    product migration file edited

--- database/migrations/2025_03_23_143343_create_products_table.php
+++ database/migrations/2025_03_23_143343_create_products_table.php
@@ -15,21 +15,42 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+
+            // General fields for parent products only
+            $table->string('name')->nullable(); // nullable for variant
             $table->text('description')->nullable();
-            $table->decimal('weight', 8, 2)->default(0.00);
-            $table->decimal('length', 8, 2)->default(0.00);
-            $table->decimal('width', 8, 2)->default(0.00);
-            $table->decimal('height', 8, 2)->default(0.00);
+            $table->text('slug')->nullable();
+            $table->decimal('weight', 8, 2)->nullable();
+            $table->decimal('length', 8, 2)->nullable();
+            $table->decimal('width', 8, 2)->nullable();
+            $table->decimal('height', 8, 2)->nullable();
+
+            // Common fields for all products and variants
             $table->integer('stock')->default(0);
             $table->string('sku')->unique();
-            $table->decimal('price', 10, 2);
+            $table->decimal('price', 10, 2)->nullable();
+
+            // Only parent products have variants
             $table->boolean('has_variants')->default(false);
-            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+
+            // Only read json columns
+            $table->json('specifications')->nullable();
+            $table->json('reviews')->nullable();
+
+            // Selectable options
+            $table->json('warranties')->nullable();
+            $table->json('policies')->nullable();
+
+            // Only parent products have category
+            $table->foreignId('category_id')->nullable()->constrained()->onDelete('cascade');
+
             $table->boolean('is_active')->default(true);
+
+            // Add NestedSet fields (parent_id, _lft, _rgt)
+            \Kalnoy\Nestedset\NestedSet::columns($table);
+
             $table->timestamps();
         });
-
     }
 
     /**
